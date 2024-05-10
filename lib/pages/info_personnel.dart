44b33ba291e8c4f5d73bf3d49782e3e1c38e2508
abderrahmanesabkari <abderrahmanesabkari@gmail.com>
2024-05-10 20:15:23 +0100

    change infopersonnel

--- lib/pages/info_personnel.dart
+++ lib/pages/info_personnel.dart
@@ -12,151 +12,226 @@ class InfoPersonnel extends StatefulWidget {
 }
 
 class _InfoPersonnelState extends State<InfoPersonnel> {
+  late TextEditingController _nomTextEditingController;
+  late TextEditingController _prenomTextEditingController;
+  late TextEditingController _telTextEditingController;
+  late TextEditingController _emailTextEditingController;
+  late TextEditingController _passwordTextEditingController;
+
+  void initState() {
+    super.initState();
+    _nomTextEditingController = TextEditingController();
+    _prenomTextEditingController = TextEditingController();
+    _telTextEditingController = TextEditingController();
+    _emailTextEditingController = TextEditingController();
+    _passwordTextEditingController = TextEditingController();
+  }
+
+  void dispose() {
+    _nomTextEditingController.dispose();
+    _prenomTextEditingController.dispose();
+    _telTextEditingController.dispose();
+    _emailTextEditingController.dispose();
+    _passwordTextEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(223, 245, 241, 1.0),
+      appBar: AppBar(
+        centerTitle: true,
         backgroundColor: const Color.fromRGBO(223, 245, 241, 1.0),
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: const Color.fromRGBO(223, 245, 241, 1.0),
-          title: Image.asset('assets/images/pogo.png', width: 120, height: 60),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(13.0),
+        title: Image.asset('assets/images/pogo.png', width: 120, height: 60),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.person_outline, size: 35),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'nom',
-                      ),
-                    ),
-                  ),
-                ],
+              const Text(
+                "Mettez à jour votre profil",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
               ),
-              const SizedBox(height: 20),
-              const Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(null, size: 35),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'prenom',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.phone_outlined, size: 35),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextField(
-                      keyboardType: TextInputType.phone,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'numero de telephone',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.email_outlined, size: 35),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'email',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(null, size: 35),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextField(
-                      keyboardType: TextInputType.visiblePassword,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'mot de passe',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Perform login/authentication logic here
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white, // Background color
-                      // Button padding
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        side: const BorderSide(
-                          color: Color.fromRGBO(5, 12, 79, 1.0),
-                          width: 2.0,
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _nomTextEditingController,
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          filled: true,
+                          fillColor: const Color.fromRGBO(180, 233, 230, 1.0),
+                          labelText: 'Votre nom',
+                          hintText: 'nom',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          prefixIcon: const Padding(
+                            padding:
+                                EdgeInsets.all(8), // Adjust padding as needed
+                            child: Icon(
+                              Icons.person, // Icon for email input
+                              color: Color.fromRGBO(
+                                  5, 12, 79, 1.0), // Color of the icon
+                              size: 25, // Size of the icon
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                    child: const Text(
-                      'Réinitialiser',
-                      style: TextStyle(
-                          color: Color.fromRGBO(5, 12, 79, 1.0)), // Text color
+                    const SizedBox(
+                        width:
+                            16), // Add some spacing between the two text fields
+                    Expanded(
+                      child: TextField(
+                        controller: _prenomTextEditingController,
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 10),
+                          filled: true,
+                          fillColor: const Color.fromRGBO(180, 233, 230, 1.0),
+                          labelText: 'Votre prenom',
+                          hintText: 'prenom',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: TextField(
+                  controller: _telTextEditingController,
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 10),
+                    filled: true,
+                    fillColor: const Color.fromRGBO(180, 233, 230, 1.0),
+                    labelText: 'Enter votre telephone',
+                    hintText: '06xxxxxxxx',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.all(8), // Adjust padding as needed
+                      child: Icon(
+                        Icons.phone, // Icon for email input
+                        color:
+                            Color.fromRGBO(5, 12, 79, 1.0), // Color of the icon
+                        size: 25, // Size of the icon
+                      ),
                     ),
                   ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  FilledButton(
-                    onPressed: () {},
-                    style: const ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(
-                            Color.fromRGBO(5, 12, 79, 1))),
-                    child: const Text(
-                      "Enregistrer",
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+                ),
+              ),
+              // const SizedBox(height: 5),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: TextField(
+                  controller: _emailTextEditingController,
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 10),
+                    filled: true,
+                    fillColor: const Color.fromRGBO(180, 233, 230, 1.0),
+                    labelText: 'Enter votre email',
+                    hintText: 'example@example.com',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.all(8), // Adjust padding as needed
+                      child: Icon(
+                        Icons.email, // Icon for email input
+                        color:
+                            Color.fromRGBO(5, 12, 79, 1.0), // Color of the icon
+                        size: 25, // Size of the icon
+                      ),
                     ),
                   ),
-                ],
-              )
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: TextField(
+                  controller: _passwordTextEditingController,
+                  obscureText: true,
+                  textInputAction: TextInputAction.done,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 10),
+                    filled: true,
+                    fillColor: const Color.fromRGBO(180, 233, 230, 1.0),
+                    labelText: 'Enter votre password',
+                    hintText: 'password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                    prefixIcon: const Padding(
+                      padding: EdgeInsets.all(8), // Adjust padding as needed
+                      child: Icon(
+                        Icons.remove_red_eye, // Icon for email input
+                        color:
+                            Color.fromRGBO(5, 12, 79, 1.0), // Color of the icon
+                        size: 25, // Size of the icon
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  // Perform login/authentication logic here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white, // Background color
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 60, vertical: 15), // Button padding
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    side: const BorderSide(
+                      color: Color.fromRGBO(5, 12, 79, 1.0),
+                      width: 2.0,
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                      color: Color.fromRGBO(5, 12, 79, 1.0)), // Text color
+                ),
+              ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
