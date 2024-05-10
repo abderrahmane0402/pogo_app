904a8b6b81f7bf860c6df6935c25e73dcb04ce51
ouajih_zakia <zakiaouajih12@gmail.com>
2024-05-10 22:05:53 +0100

    jefnhejnf

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,11 +6,80 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Hello, World!',
-        style: TextStyle(fontSize: 24),
+    return  Padding(
+      padding: const EdgeInsets.only(top: 10.0), // Adjust the value as needed
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, // Align children to the top of the column
+        children: [
+          SizedBox(
+            height: 230,
+            width: 350,
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(5, 12, 79, 1.0),
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              alignment: Alignment.topCenter, // Align the child to the top
+              child:  Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children:[
+                  const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding:  EdgeInsets.only(left: 5.0), // Add padding to the top
+                        child: SizedBox(
+                          width: 150,
+                          child: Text(
+                            'Votre sécurité est notre priorité.',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:  EdgeInsets.only(left: 5.0),
+                      child: SizedBox(
+                        width: 150, // Set your desired width here
+                        child: Divider(
+                          height: 20, // Set your desired height for the line
+                          thickness: 2, // Set your desired thickness for the line
+                          color: Colors.white, // Set your desired color for the line
+                        ),
+                      ),),
+                      Padding(
+                        padding:  EdgeInsets.only(left: 5.0),
+                      child: SizedBox(
+                        width: 150, // Set your desired width here
+                        child: Text(
+                          'Profitez de transactions sûres avec notre application mobile',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w100,
+                            fontSize: 12
+                          ),
+                        ),
+                      ),),
+                    ],
+                  ),
+
+                   Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    // Set your desired height here
+                      child: Image.asset('assets/images/home.png', width: 170),
+
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Add more widgets here if needed
+        ],
       ),
     );
+
+
   }
 }
