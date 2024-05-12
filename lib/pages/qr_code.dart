e6bc3bdd4f2f92300e530753c59e7f5cfbc278eb
ouajih_zakia <zakiaouajih12@gmail.com>
2024-05-12 21:48:41 +0100

    kjcjken

--- lib/pages/qr_code.dart
+++ lib/pages/qr_code.dart
@@ -1,25 +1,102 @@
-// Importing necessary libraries
-import 'package:flutter/material.dart';
+import 'dart:io'; // Import necessary library for Platform
 
-// Define the main function, which is the entry point of the Dart application
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
 void main() {
-  // Call the runApp function to start the Flutter application
   runApp(const QR_Code());
 }
 
-// Define a StatelessWidget to represent your app
-class QR_Code extends StatelessWidget {
-  const QR_Code({super.key});
+class QR_Code extends StatefulWidget {
+  const QR_Code({Key? key}) : super(key: key);
 
-  // Override the build method to describe the UI of your app
+  @override
+  _QRViewExampleState createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QR_Code> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Return a MaterialApp widget, which is the root of your app's widget tree
-    return const Center(
-      child: Text(
-        'Hello, World!',
-        style: TextStyle(fontSize: 24),
+    return  Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(flex: 4, child: _buildQrView(context)),
+          Expanded(
+            flex: 1,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  if (result != null)
+                    Text(
+                        'Barcode Type: ${result!.format.name}   Data: ${result!.code}')
+                  else
+                    const Text('Scan a code'),
+
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+        MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('No Permission to access the camera')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
