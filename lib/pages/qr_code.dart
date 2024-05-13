e9398c19fe1efcddb721f0d99c0c7aed9bdcf359
ouajih_zakia <zakiaouajih12@gmail.com>
2024-05-13 01:13:43 +0100

    scnjkndc

--- lib/pages/qr_code.dart
+++ lib/pages/qr_code.dart
@@ -8,95 +8,15 @@ void main() {
   runApp(const QR_Code());
 }
 
-class QR_Code extends StatefulWidget {
+class QR_Code extends StatelessWidget {
   const QR_Code({Key? key}) : super(key: key);
 
   @override
-  _QRViewExampleState createState() => _QRViewExampleState();
-}
-
-class _QRViewExampleState extends State<QR_Code> {
-  Barcode? result;
-  QRViewController? controller;
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    }
-    controller!.resumeCamera();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Column(
-        children: <Widget>[
-          Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(
-            flex: 1,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  if (result != null)
-                    Text(
-                        'Barcode Type: ${result!.format.name}   Data: ${result!.code}')
-                  else
-                    const Text('Scan a code'),
+    return const Padding(
+        padding: EdgeInsets.only(
+        top: 10.0, left: 3.0, right: 3.0),);}
 
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
 
-  Widget _buildQrView(BuildContext context) {
-    var scanArea = (MediaQuery.of(context).size.width < 400 ||
-        MediaQuery.of(context).size.height < 400)
-        ? 150.0
-        : 300.0;
-    return QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-      overlay: QrScannerOverlayShape(
-          borderColor: Colors.red,
-          borderRadius: 10,
-          borderLength: 30,
-          borderWidth: 10,
-          cutOutSize: scanArea),
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
-    );
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-    });
-  }
-
-  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    if (!p) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('No Permission to access the camera')),
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
 }
+
