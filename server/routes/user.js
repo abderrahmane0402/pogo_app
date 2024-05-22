b372a41da12da750c3154ee230d73e24b8abdddb
abderrahmanesabkari <abderrahmanesabkari@gmail.com>
2024-05-22 14:18:34 +0100

    finish carte operation

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,23 +4,9 @@ import { authenticateToken, generateAccessToken } from "../middleware.js"
 import { body, param, validationResult } from "express-validator"
 const router = Router()
 
-// data validator for getUser
-const getUserValidator = [
-  param("id").trim().notEmpty(),
-  (req, res, next) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res
-        .status(400)
-        .json({ message: "Validation failed", errors: errors.array() })
-    }
-    next()
-  },
-]
-
 // getUser
-router.get("/:id", authenticateToken, async (req, res) => {
-  const id = req.params.id
+router.get("/", authenticateToken, async (req, res) => {
+  const { id } = req.user
   try {
     const user = await Utilisateur.findById(id).select([
       "-password",
@@ -56,42 +42,36 @@ const userValidator = [
 ]
 
 // update user
-router.put(
-  "/update/:id",
-  authenticateToken,
-  userValidator,
-  async (req, res) => {
-    const id = req.params.id
-    const { nom, prenom, telephone } = req.body
-    try {
-      // Find the user by ID and update the fields
-      const updatedUser = await Utilisateur.findByIdAndUpdate(
-        id,
-        {
-          nom,
-          prenom,
-          telephone,
-        },
-        { new: true, runValidators: true } // Return the updated document and run validators
-      )
+router.put("/update", authenticateToken, userValidator, async (req, res) => {
+  const { id } = req.user
+  const { nom, prenom, telephone } = req.body
+  try {
+    // Find the user by ID and update the fields
+    const updatedUser = await Utilisateur.findByIdAndUpdate(
+      id,
+      {
+        nom,
+        prenom,
+        telephone,
+      },
+      { new: true, runValidators: true } // Return the updated document and run validators
+    )
 
-      if (!updatedUser) {
-        return res
-          .status(404)
-          .json({ message: "Utilisateur non trouvé", status: "error" })
-      }
-
-      res.json({ status: "success", message: "User updated successfully" })
-    } catch (error) {
-      console.error(error.message)
-      res.status(500).json({ message: error.message, status: "error" })
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .json({ message: "Utilisateur non trouvé", status: "error" })
     }
+
+    res.json({ status: "success", message: "User updated successfully" })
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ message: error.message, status: "error" })
   }
-)
+})
 
 // data validator for carte bancaire
 const carteValidator = [
-  param("id").trim().notEmpty(),
   body("nomProprietaire").trim().notEmpty(),
   body("isdefault").trim().notEmpty().isBoolean(),
   body("numCarte").trim().notEmpty().isNumeric(),
@@ -111,14 +91,14 @@ const carteValidator = [
 // add carte bancaire
 
 router.post(
-  "/addCarte/:id",
+  "/addCarte",
   authenticateToken,
   carteValidator,
   async (req, res) => {
     try {
       const { nomProprietaire, numCarte, cvv, dateExperation, isdefault } =
         req.body
-      const { id } = req.params
+      const { id } = req.user
 
       // Find the user by ID
       const utilisateur = await Utilisateur.findById(id)
@@ -166,14 +146,56 @@ router.post(
 )
 
 // get default carte bancaire
-router.get(
-  "/defaultCarte/:id",
+router.get("/defaultCarte", authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.user
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ message: "Validation failed", errors: errors.array() })
+    }
+
+    // Find the user by ID
+    const utilisateur = await Utilisateur.findById(id).select("carteBancaire")
+    if (!utilisateur) {
+      return res
+        .status(404)
+        .json({ message: "Utilisateur non trouvé", status: "error" })
+    }
+    const carteNumber = utilisateur.carteBancaire.length
+    if (carteNumber == 0) {
+      return res
+        .status(404)
+        .json({ message: "ajouter une carte bancaire", status: "error" })
+    }
+
+    const defaultCard = utilisateur.carteBancaire.find(
+      (carte) => carte.isdefault
+    )
+    if (!defaultCard) {
+      return res.status(404).json({ message: "Carte bancaire non trouvée" })
+    }
+
+    res.status(201).json({ carte: defaultCard, status: "success" })
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ message: error.message, status: "error" })
+  }
+})
+
+// delete carte bancaire
+router.delete(
+  "/deleteCarte/:id",
+  authenticateToken,
   param("id").trim().notEmpty(),
   async (req, res) => {
     try {
-      const { id } = req.params
-
       const errors = validationResult(req)
+      const { id } = req.user
+      const carteID = req.params.id
+
       if (!errors.isEmpty()) {
         return res
           .status(400)
@@ -187,47 +209,27 @@ router.get(
           .status(404)
           .json({ message: "Utilisateur non trouvé", status: "error" })
       }
-      const carteNumber = utilisateur.carteBancaire.length
-      if (carteNumber == 0) {
+
+      const carte = utilisateur.carteBancaire.id(carteID)
+      if (!carte) {
         return res
           .status(404)
-          .json({ message: "ajouter une carte bancaire", status: "error" })
+          .json({ message: "Carte non trouvé", status: "error" })
       }
 
-      const defaultCard = utilisateur.carteBancaire.find(
-        (carte) => carte.isdefault
-      )
-      if (!defaultCard) {
-        return res.status(404).json({ message: "Carte bancaire non trouvée" })
+      // Remove the carte from the user's cartes array
+      carte.deleteOne()
+
+      // change default carte to the fisrt carte
+      const carteNumber = utilisateur.carteBancaire.length
+      if (carteNumber != 0) {
+        utilisateur.carteBancaire[0].isdefault = true
       }
+      await utilisateur.save()
 
-      res.status(201).json({ carte: defaultCard, status: "success" })
-    } catch (error) {
-      console.error(error.message)
-      res.status(500).json({ message: error.message, status: "error" })
-    }
-  }
-)
-
-// delete carte bancaire
-router.delete(
-  "/deleteCarte/:id",
-  param("id").trim().notEmpty(),
-  async (req, res) => {
-    try {
-      const { id } = req.params
-
-      const errors = validationResult(req)
-      if (!errors.isEmpty()) {
-        return res
-          .status(400)
-          .json({ message: "Validation failed", errors: errors.array() })
-      }
-
-      // Find the user by ID
-      
-
-      res.status(201).json({ carte: defaultCard, status: "success" })
+      res
+        .status(201)
+        .json({ message: "Carte supprimée avec succès", status: "success" })
     } catch (error) {
       console.error(error.message)
       res.status(500).json({ message: error.message, status: "error" })
