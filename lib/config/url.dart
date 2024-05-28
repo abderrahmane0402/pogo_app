e837ff3407437e981a6f7aed9c4ece112c0f9376
abderrahmanesabkari <abderrahmanesabkari@gmail.com>
2024-05-28 12:05:13 +0100

    jell

--- lib/config/url.dart
+++ lib/config/url.dart
@@ -1,5 +1,5 @@
-final url = "http://192.168.43.102:3000/";
-final register_url = url +"auth/registre";
+final url = "http://192.168.0.155:3000/";
+final register_url = url + "auth/registre";
 final login_url = url + "auth/login";
 final add_cart = url + "user/addCarte";
 final default_cart = url + "user/defaultCarte";
